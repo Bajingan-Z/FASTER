9fc94ffacaa54ce9d0c160ac27efe814e006c59d
Bangsat Militan☆ <95204908+Bajingan-Z@users.noreply.github.com>
2021-11-29 15:18:12 +0700

    Create My.js

---
+++ My.js
@@ -0,0 +1,12 @@
+rm -rf BAJINGAN_Z
+apt update && apt upgrade
+pkg install python -y
+apt install python2
+pkg install python3
+pip2 install bs4
+pkg install requests
+pip2 install mechanize
+apt install git y
+git clone https://github.com/Bajingan-Z/BAJINGAN_Z
+cd BAJINGAN_Z
+python2 BAJINGAN_Z.py

